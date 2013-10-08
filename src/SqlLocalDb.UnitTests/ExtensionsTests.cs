3420f8b9302e9b8750faa6f2d807dbc0bf9c3546
martincostello <martin.costello1@gmail.com>
2013-10-08 23:10:24 +0100

    Added extension methods to make it easier to use the library with EntityFramework.

---
+++ src/SqlLocalDb.UnitTests/ExtensionsTests.cs
@@ -0,0 +1,132 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ExtensionsTests.cs" company="http://sqllocaldb.codeplex.com">
+//   Martin Costello (c) 2012-2013
+// </copyright>
+// <license>
+//   See license.txt in the project root for license information.
+// </license>
+// <summary>
+//   ExtensionsTests.cs
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Data.Common;
+using System.Data.EntityClient;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace System.Data.SqlLocalDb
+{
+    /// <summary>
+    /// A class containing unit tests for the <see cref="Extensions"/> class.
+    /// </summary>
+    [TestClass]
+    public class ExtensionsTests
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExtensionsTests"/> class.
+        /// </summary>
+        public ExtensionsTests()
+        {
+        }
+
+        #endregion
+
+        #region Methods
+
+        [TestMethod]
+        [Description("Tests GetConnectionForModel() if instance is null.")]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void GetConnectionForModel_Throws_If_Instance_Is_Null()
+        {
+            // Arrange
+            ISqlLocalDbInstance instance = null;
+            string modelConnectionStringName = "MyConnectionString";
+
+            // Act and Assert
+            throw ErrorAssert.Throws<ArgumentNullException>(
+                () => instance.GetConnectionForModel(modelConnectionStringName),
+                "instance");
+        }
+
+        [TestMethod]
+        [Description("Tests GetConnectionForModel() returns a connection.")]
+        public void GetConnectionForModel_Returns_Connection()
+        {
+            // Arrange
+            string namedPipe = @"np:\\.\pipe\LOCALDB#C0209E6A\tsql\query";
+
+            Mock<ISqlLocalDbInstance> mock = new Mock<ISqlLocalDbInstance>();
+            mock.Setup((p) => p.NamedPipe).Returns(namedPipe);
+
+            ISqlLocalDbInstance instance = mock.Object;
+            string modelConnectionStringName = "MyDataContext";
+
+            // Act
+            using (DbConnection result = instance.GetConnectionForModel(modelConnectionStringName))
+            {
+                // Assert
+                Assert.IsNotNull(result, "GetConnectionForModel() returned null.");
+                Assert.IsInstanceOfType(result, typeof(EntityConnection), "GetConnectionForModel() returned incorrect type.");
+                StringAssert.Contains(result.ConnectionString, namedPipe, "The named pipe is not present in the connection string.");
+                Assert.AreEqual(ConnectionState.Closed, result.State, "DbConnection.State is incorrect.");
+            }
+        }
+
+        [TestMethod]
+        [Description("Tests GetConnectionStringForModel() if instance is null.")]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void GetConnectionStringForModel_Throws_If_Instance_Is_Null()
+        {
+            // Arrange
+            ISqlLocalDbInstance instance = null;
+            string modelConnectionStringName = "MyConnectionString";
+
+            // Act and Assert
+            throw ErrorAssert.Throws<ArgumentNullException>(
+                () => instance.GetConnectionStringForModel(modelConnectionStringName),
+                "instance");
+        }
+
+        [TestMethod]
+        [Description("Tests GetConnectionStringForModel() if modelConnectionStringName cannot be found.")]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetConnectionStringForModel_Throws_If_ConnectionString_Cannot_Be_Found()
+        {
+            // Arrange
+            ISqlLocalDbInstance instance = Mock.Of<ISqlLocalDbInstance>();
+            string modelConnectionStringName = "NonExistentContext";
+
+            // Act and Assert
+            throw ErrorAssert.Throws<ArgumentNullException>(
+                () => instance.GetConnectionStringForModel(modelConnectionStringName),
+                "modelConnectionStringName");
+        }
+
+        [TestMethod]
+        [Description("Tests GetConnectionStringForModel() returns a connection string.")]
+        public void GetConnectionStringForModel_Returns_Connection_String()
+        {
+            // Arrange
+            string namedPipe = @"np:\\.\pipe\LOCALDB#C0209E6A\tsql\query";
+
+            Mock<ISqlLocalDbInstance> mock = new Mock<ISqlLocalDbInstance>();
+            mock.Setup((p) => p.NamedPipe).Returns(namedPipe);
+
+            ISqlLocalDbInstance instance = mock.Object;
+            string modelConnectionStringName = "MyDataContext";
+
+            // Act
+            DbConnectionStringBuilder result = instance.GetConnectionStringForModel(modelConnectionStringName);
+
+            // Assert
+            Assert.IsNotNull(result, "GetConnectionStringForModel() returned null.");
+            Assert.IsInstanceOfType(result, typeof(EntityConnectionStringBuilder), "GetConnectionStringForModel() returned incorrect type.");
+            StringAssert.Contains(result.ConnectionString, namedPipe, "The named pipe is not present in the connection string.");
+        }
+
+        #endregion
+    }
+}
---
+++ src/SqlLocalDb/Extensions.cs
@@ -0,0 +1,93 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Extensions.cs" company="http://sqllocaldb.codeplex.com">
+//   Martin Costello (c) 2012-2013
+// </copyright>
+// <license>
+//   See license.txt in the project root for license information.
+// </license>
+// <summary>
+//   Extensions.cs
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Configuration;
+using System.Data.Common;
+using System.Data.EntityClient;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace System.Data.SqlLocalDb
+{
+    /// <summary>
+    /// A class containing extension methods for use with SQL LocalDB instances.  This class cannot be inherited.
+    /// </summary>
+    [System.ComponentModel.EditorBrowsable(System.ComponentModel.EditorBrowsableState.Never)]
+    public static class Extensions
+    {
+        #region Methods
+
+        /// <summary>
+        /// Creates an instance of <see cref="DbConnection"/> that can be used to connect to the SQL LocalDB instance.
+        /// </summary>
+        /// <param name="instance">The <see cref="ISqlLocalDbInstance"/> to get the connection string builder for.</param>
+        /// <param name="modelConnectionStringName">The name of the connection string for the model in the application configuration file.</param>
+        /// <returns>
+        /// The created instance of <see cref="DbConnectionStringBuilder"/>.
+        /// </returns>
+        /// <exception cref="ArgumentException">
+        /// No connection string with the name specified by <paramref name="modelConnectionStringName"/> can be found in the application configuration file.
+        /// </exception>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="instance"/> is <see langword="null"/>.
+        /// </exception>
+        public static DbConnection GetConnectionForModel(this ISqlLocalDbInstance instance, string modelConnectionStringName)
+        {
+            DbConnectionStringBuilder builder = instance.GetConnectionStringForModel(modelConnectionStringName);
+            return new EntityConnection(builder.ConnectionString);
+        }
+
+        /// <summary>
+        /// Creates an instance of <see cref="DbConnectionStringBuilder"/> that can be used to connect to the SQL LocalDB instance.
+        /// </summary>
+        /// <param name="instance">The <see cref="ISqlLocalDbInstance"/> to get the connection string builder for.</param>
+        /// <param name="modelConnectionStringName">The name of the connection string for the model in the application configuration file.</param>
+        /// <returns>
+        /// The created instance of <see cref="DbConnectionStringBuilder"/>.
+        /// </returns>
+        /// <exception cref="ArgumentException">
+        /// No connection string with the name specified by <paramref name="modelConnectionStringName"/> can be found in the application configuration file.
+        /// </exception>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="instance"/> is <see langword="null"/>.
+        /// </exception>
+        public static DbConnectionStringBuilder GetConnectionStringForModel(this ISqlLocalDbInstance instance, string modelConnectionStringName)
+        {
+            if (instance == null)
+            {
+                throw new ArgumentNullException("instance");
+            }
+
+            var connectionStringSettings = ConfigurationManager.ConnectionStrings
+                .OfType<ConnectionStringSettings>()
+                .Where((p) => string.Equals(p.Name, modelConnectionStringName, StringComparison.Ordinal))
+                .FirstOrDefault();
+
+            if (connectionStringSettings == null)
+            {
+                throw new ArgumentException(SRHelper.Format(SR.Extensions_NoConnectionStringFormat, modelConnectionStringName), "modelConnectionStringName");
+            }
+            
+            EntityConnectionStringBuilder entityBuilder = new EntityConnectionStringBuilder(connectionStringSettings.ConnectionString);
+
+            SqlConnectionStringBuilder providerBuilder = new SqlConnectionStringBuilder(entityBuilder.ProviderConnectionString)
+            {
+                DataSource = instance.NamedPipe,
+            };
+
+            entityBuilder.ProviderConnectionString = providerBuilder.ConnectionString;
+            return entityBuilder;
+        }
+
+        #endregion
+    }
+}
