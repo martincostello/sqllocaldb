99f9b194b591f767526ac662a4028d0a9da98221
martincostello_cp <SND\martincostello_cp>
2013-10-07 23:58:17 -0700

    Changed to use the Apache 2.0 licence.

--- Trunk/src/SqlLocalDb.UnitTests/SqlLocalDbApiTests.cs
+++ Trunk/src/SqlLocalDb.UnitTests/SqlLocalDbApiTests.cs
@@ -1,13 +1,9 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="SqlLocalDbApiTests.cs" company="http://sqllocaldb.codeplex.com">
-//   New BSD License (BSD)
+//   Martin Costello (c) 2012-2013
 // </copyright>
 // <license>
-//   This source code is subject to terms and conditions of the New BSD Licence (BSD). 
-//   A copy of the license can be found in the License.txt file at the root of this
-//   distribution.  By using this source code in any fashion, you are agreeing to be
-//   bound by the terms of the New BSD Licence. You must not remove this notice, or
-//   any other, from this software.
+//   See license.txt in the project root for license information.
 // </license>
 // <summary>
 //   SqlLocalDbApiTests.cs
