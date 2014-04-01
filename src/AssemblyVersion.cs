9076e34b7089bd87d6be3651cf57ce09d74cd8de
martincostello <martin@martincostello.com>
2014-04-02 00:43:18 +0100

    Updated version to 1.9.0.0.

--- src/AssemblyVersion.cs
+++ src/AssemblyVersion.cs
@@ -12,6 +12,6 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.8.0.0")]
-[assembly: AssemblyFileVersion("1.8.0.0")]
-[assembly: AssemblyInformationalVersion("1.8.0.0")]
+[assembly: AssemblyVersion("1.9.0.0")]
+[assembly: AssemblyFileVersion("1.9.0.0")]
+[assembly: AssemblyInformationalVersion("1.9.0.0")]
