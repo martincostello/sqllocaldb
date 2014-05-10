92c1a5be098d3bb5b2f933ad676ca085fa0099e1
martincostello <martin@martincostello.com>
2014-05-10 12:47:04 +0100

    The EFSample project now uses its own derived DbConfiguration instance. This provides some existing infrastructure to use to do research.

--- src/SqlLocalDb.EFSample/BlogDbConfiguration.cs
+++ src/SqlLocalDb.EFSample/BlogDbConfiguration.cs
@@ -1,36 +1,24 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="BlogDbContext.cs" company="http://sqllocaldb.codeplex.com">
+// <copyright file="BlogDbConfiguration.cs" company="http://sqllocaldb.codeplex.com">
 //   Martin Costello (c) 2012-2014
 // </copyright>
 // <license>
 //   See license.txt in the project root for license information.
 // </license>
 // <summary>
-//   BlogDbContext.cs
+//   Blog.cs
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
 
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 
 namespace System.Data.SqlLocalDb
 {
-    [DbConfigurationType(typeof(BlogDbConfiguration))]
-    public class BlogDbContext : DbContext
+    public sealed class BlogDbConfiguration : DbConfiguration
     {
-        public BlogDbContext()
+        public BlogDbConfiguration()
             : base()
         {
         }
-
-        public BlogDbContext(string nameOrConnectionString)
-            : base(nameOrConnectionString)
-        {
-        }
-
-        public DbSet<Blog> Blogs { get; set; }
-
-        public DbSet<Post> Posts { get; set; }
     }
 }
