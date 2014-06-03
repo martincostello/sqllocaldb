a124edbfcb3fdf10ede89186fc0db8f01f2435c3
martincostello <martin@martincostello.com>
2014-06-04 00:30:06 +0100

    Added AssemblyInitialize() which uses the SqlLocalDbApi.StopOptions property set to NoWait to significantly reduce the overall duration of the test runs.

---
+++ src/SqlLocalDb.UnitTests/TestSetup.cs
@@ -0,0 +1,48 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="TestSetup.cs" company="http://sqllocaldb.codeplex.com">
+//   Martin Costello (c) 2012-2014
+// </copyright>
+// <license>
+//   See license.txt in the project root for license information.
+// </license>
+// <summary>
+//   TestSetup.cs
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace System.Data.SqlLocalDb
+{
+    /// <summary>
+    /// A class representing the setup class for the test assembly.  This class cannot be inherited.
+    /// </summary>
+    [TestClass]
+    public sealed class TestSetup
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Prevents a default instance of the <see cref="TestSetup"/> class from being created.
+        /// </summary>
+        private TestSetup()
+        {
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Initializes the test assembly.
+        /// </summary>
+        /// <param name="context">The test context.</param>
+        [AssemblyInitialize]
+        public static void AssemblyInitialize(TestContext context)
+        {
+            SqlLocalDbApi.StopOptions = StopInstanceOptions.NoWait;
+        }
+
+        #endregion
+    }
+}
