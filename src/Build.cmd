217807cd8c79cb777f2687fb7d9cb29a6f8f3aca
martincostello <martin@martincostello.com>
2015-03-31 18:39:09 +0100

    Build.cmd now correctly builds with the correct version of MSBuild if only Visual Studio 2015 is installed.

--- src/Build.cmd
+++ src/Build.cmd
@@ -1,3 +1,9 @@
-@ECHO OFF
+@echo off
 
-"%ProgramFiles(x86)%\MSBuild\12.0\Bin\msbuild" "%~dp0\SqlLocalDb.msbuild" /v:minimal /maxcpucount /nodeReuse:false %*
+set _MSBuild="%ProgramFiles(x86)%\MSBuild\12.0\Bin\MSBuild.exe"
+if not exist %_MSBuild% set _MSBuild="%ProgramFiles%\MSBuild\12.0\Bin\MSBuild.exe"
+if not exist %_MSBuild% set _MSBuild="%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe"
+if not exist %_MSBuild% set _MSBuild="%ProgramFiles%\MSBuild\14.0\Bin\MSBuild.exe"
+if not exist %_MSBuild% echo Error: Could not find MSBuild.exe. && exit /b 1
+
+%_MSBuild% "%~dp0\SqlLocalDb.msbuild" /v:minimal /maxcpucount /nodeReuse:false %*
