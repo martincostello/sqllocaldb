0de04ae4abe482ffe30cb762dd734c84b00d29c0
martincostello <martin@martincostello.com>
2015-03-31 18:29:20 +0100

    Fixed Build.cmd not working on a machine where only Visual Studio 2015 is installed.

--- src/Build.cmd
+++ src/Build.cmd
@@ -1,3 +1,3 @@
 @ECHO OFF
 
-"%ProgramFiles(x86)%\MSBuild\12.0\Bin\msbuild" "%~dp0\SqlLocalDb.msbuild" /v:minimal /maxcpucount /nodeReuse:false %*
+"%ProgramFiles(x86)%\MSBuild\14.0\Bin\msbuild" "%~dp0\SqlLocalDb.msbuild" /v:minimal /maxcpucount /nodeReuse:false %*
