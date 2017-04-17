06f17b79c67bc96bee1287e1e76ddfe5198ef6bc
martincostello <martin@martincostello.com>
2017-04-17 20:39:09 +0100

    Restore AssemblyVersion.cs
    Restore AssemblyVersion.cs back for patching the assembly version and
    metadata.

---
+++ AssemblyVersion.cs
@@ -0,0 +1,17 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CommonAssemblyInfo.cs" company="https://github.com/martincostello/sqllocaldb">
+//   Martin Costello (c) 2012-2015
+// </copyright>
+// <license>
+//   See license.txt in the project root for license information.
+// </license>
+// <summary>
+//   CommonAssemblyInfo.cs
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Reflection;
+
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyInformationalVersion("2.0.0.0")]
