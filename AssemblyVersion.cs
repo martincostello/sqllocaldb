00f8983e329d6931514610f7e5407992afe6d705
martincostello <martin@martincostello.com>
2017-04-17 20:57:58 +0100

    Fix incorrect file header
    Fix copy-paste mistake in file header.

--- AssemblyVersion.cs
+++ AssemblyVersion.cs
@@ -1,12 +1,12 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="CommonAssemblyInfo.cs" company="https://github.com/martincostello/sqllocaldb">
+// <copyright file="AssemblyVersion.cs" company="https://github.com/martincostello/sqllocaldb">
 //   Martin Costello (c) 2012-2015
 // </copyright>
 // <license>
 //   See license.txt in the project root for license information.
 // </license>
 // <summary>
-//   CommonAssemblyInfo.cs
+//   AssemblyVersion.cs
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
 
