9744a2b3c928105ce6dbcf0994f1ad133e49b327
martincostello <martin@martincostello.com>
2018-08-05 14:26:09 +0100

    Fix another missing PNSE
    Add PlatformNotSupportedException for non-Windows OS when trying to delete an instance.

--- src/SqlLocalDb/SqlLocalDbApi.cs
+++ src/SqlLocalDb/SqlLocalDbApi.cs
@@ -1115,6 +1115,8 @@ namespace MartinCostello.SqlLocalDb
                 throw new ArgumentNullException(nameof(instanceName));
             }
 
+            EnsurePlatformSupported();
+
             Logger.DeletingInstance(instanceName);
 
             int hr = _api.DeleteInstance(instanceName, ReservedValue);
