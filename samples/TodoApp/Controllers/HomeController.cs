e8fc37719a4c8887204180dad07401dbb7e14658
martincostello <martin@martincostello.com>
2018-08-11 13:22:24 +0100

    Add sample application shell
    Add a shell sample application that will demonstrate a TODO application backed by SQL LocalDB.

---
+++ samples/TodoApp/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Martin Costello, 2012-2018. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+using System.Diagnostics;
+using MartinCostello.SqlLocalDb;
+using Microsoft.AspNetCore.Mvc;
+using TodoApp.Models;
+
+namespace TodoApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ISqlLocalDbApi _localDB;
+
+        public HomeController(ISqlLocalDbApi localDB)
+        {
+            _localDB = localDB;
+        }
+
+        public IActionResult Index() => View();
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ samples/TodoApp/Startup.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) Martin Costello, 2012-2018. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+using MartinCostello.SqlLocalDb;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TodoApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<ISqlLocalDbApi, SqlLocalDbApi>();
+
+            services.AddMvc()
+                    .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
