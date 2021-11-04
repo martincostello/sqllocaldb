251cff04f41289a2d62ab5b9f69a7aac012feef3
martincostello <martin@martincostello.com>
2021-11-04 16:10:29 +0000

    dotnet format
    Run dotnet format on the solution.

--- src/SqlLocalDb/Interop/IRegistry.cs
+++ src/SqlLocalDb/Interop/IRegistry.cs
@@ -1,4 +1,4 @@
-// Copyright (c) Martin Costello, 2012-2018. All rights reserved.
+﻿// Copyright (c) Martin Costello, 2012-2018. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
 namespace MartinCostello.SqlLocalDb.Interop;
