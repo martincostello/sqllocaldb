0b8349c307b9ab6aeecddd15d03c4f0c040e74ba
martincostello <martin@martincostello.com>
2023-10-25 19:19:03 +0100

    Use MSBuild for assembly attributes
    Remove `CommonAssemblyInfo.cs` and use MSBuild to set assembly attributes.

--- src/TestApp/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿// Copyright (c) Martin Costello, 2012-2018. All rights reserved.
-// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
-
-using System.Runtime.InteropServices;
-
-[assembly: Guid("b6649785-36c9-46d4-8f86-85bb6ee60c7e")]
