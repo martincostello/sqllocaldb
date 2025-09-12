d2f9692db56f9eef9543919bc3d78a872ff541e7
martincostello <martin@martincostello.com>
2025-09-12 15:56:53 +0100

    Resolve CA1510 warnings
    Use C# 14 extension-everything to polyfill `ArgumentNullException.ThrowIfNull()`.

---
+++ src/SqlLocalDb/ArgumentNullExceptionExtensions.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Martin Costello, 2012-2018. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+#if NETFRAMEWORK
+using System.Runtime.CompilerServices;
+
+#pragma warning disable IDE0130
+namespace System;
+
+internal static class ArgumentNullExceptionExtensions
+{
+    extension(ArgumentNullException)
+    {
+        public static void ThrowIfNull(object? argument, [CallerArgumentExpression(nameof(argument))] string? paramName = null)
+        {
+            if (argument is null)
+            {
+                throw new ArgumentNullException(paramName);
+            }
+        }
+    }
+}
+#endif
